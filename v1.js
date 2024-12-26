0478dcb2ca8390bcfe9658191e4aa0b780f68853
GJOED <58622070+GJOED-JOE@users.noreply.github.com>
2024-12-26 16:18:26 +0800

    Update v1.js

--- v1.js
+++ v1.js
@@ -3,23 +3,35 @@ let response = $response.body;
 
 // 最大值限制
 const MAX_SRE = 11650;
-
-// 更新 nsmxList 数据
-response.data.nsmxList.forEach(item => {
-    if (item.sre > MAX_SRE) {
-        // 计算倍率
-        const multiplier = MAX_SRE / item.sre;
-        // 更新 sre 和 ybtse
-        item.sre = MAX_SRE;
-        item.ybtse = parseFloat((item.ybtse * multiplier).toFixed(2));
+if (body) {
+    switch (true) {
+        // 推荐内容过滤
+        case /pgc\/season\/app\/related\/recommend\?/.test($request.url):
+            // 更新 nsmxList 数据
+            response.data.nsmxList.forEach(item => {
+                if (item.sre > MAX_SRE) {
+                    // 计算倍率
+                    const multiplier = MAX_SRE / item.sre;
+                    // 更新 sre 和 ybtse
+                    item.sre = MAX_SRE;
+                    item.ybtse = parseFloat((item.ybtse * multiplier).toFixed(2));
+                }
+            });
+            
+            // 重新统计 sreHj 和 ybtseHj
+            response.data.sreHj = response.data.nsmxList.reduce((sum, item) => sum + item.sre, 0);
+            response.data.ybtseHj = parseFloat(
+                response.data.nsmxList.reduce((sum, item) => sum + item.ybtse, 0).toFixed(2)
+            );
+            
+            // 输出修改后的数据
+            // console.log(response);
+            break;
+        default:
+            $done({});
     }
-});
+    $done({ body });
 
-// 重新统计 sreHj 和 ybtseHj
-response.data.sreHj = response.data.nsmxList.reduce((sum, item) => sum + item.sre, 0);
-response.data.ybtseHj = parseFloat(
-    response.data.nsmxList.reduce((sum, item) => sum + item.ybtse, 0).toFixed(2)
-);
-
-// 输出修改后的数据
-console.log(response);
+}else{
+    $done({});
+}
