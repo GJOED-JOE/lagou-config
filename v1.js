ad71c51406842baea144942f473074513df80411
GJOED <58622070+GJOED-JOE@users.noreply.github.com>
2024-12-26 18:32:45 +0800

    Update v1.js

--- v1.js
+++ v1.js
@@ -1,28 +1,28 @@
 // 原始数据
-let body = $response.body;
+// let body = $response.body;
 
 // 最大值限制
-const MAX_SRE = 11650;
+// const MAX_SRE = 11650;
 // if (body) {
     // switch (true) {
         // 推荐内容过滤
         // case /web\/sb\/yd\/gg\/cxNsmxList/.test($request.url):
             // 更新 nsmxList 数据
-            response.data.nsmxList.forEach(item => {
-                if (item.sre > MAX_SRE) {
-                    // 计算倍率
-                    const multiplier = MAX_SRE / item.sre;
-                    // 更新 sre 和 ybtse
-                    item.sre = MAX_SRE;
-                    item.ybtse = parseFloat((item.ybtse * multiplier).toFixed(2));
-                }
-            });
+            // response.data.nsmxList.forEach(item => {
+            //     if (item.sre > MAX_SRE) {
+            //         // 计算倍率
+            //         const multiplier = MAX_SRE / item.sre;
+            //         // 更新 sre 和 ybtse
+            //         item.sre = MAX_SRE;
+            //         item.ybtse = parseFloat((item.ybtse * multiplier).toFixed(2));
+            //     }
+            // });
             
-            // 重新统计 sreHj 和 ybtseHj
-            response.data.sreHj = response.data.nsmxList.reduce((sum, item) => sum + item.sre, 0);
-            response.data.ybtseHj = parseFloat(
-                response.data.nsmxList.reduce((sum, item) => sum + item.ybtse, 0).toFixed(2)
-            );
+            // // 重新统计 sreHj 和 ybtseHj
+            // response.data.sreHj = response.data.nsmxList.reduce((sum, item) => sum + item.sre, 0);
+            // response.data.ybtseHj = parseFloat(
+            //     response.data.nsmxList.reduce((sum, item) => sum + item.ybtse, 0).toFixed(2)
+            // );
             
             // 输出修改后的数据
             // console.log(response);
@@ -30,8 +30,8 @@ const MAX_SRE = 11650;
         // default:
             // $done({});
     // }
-    $done(body);
+    // $done(body);
 
 // }else{
-//     $done({});
+    $done({});
 // }
