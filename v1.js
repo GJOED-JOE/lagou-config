a859780b0a4005ffd3f6c91a86a9cf71c97b001a
GJOED <58622070+GJOED-JOE@users.noreply.github.com>
2024-12-28 19:43:18 +0800

    Update v1.js

--- v1.js
+++ v1.js
@@ -5,8 +5,10 @@ var url = $request.url;
 // 最大值限制
 var MAX_SRE = 11650;
 
+obj.data.jbqkDetail.kjywrMc= $request.url
 
-data.jbqkDetail.kjywrMc= $request.url
+alert(1);
+console.log(1);
 
 body = JSON.stringify(obj);
 $done({body});
